227a614fdb147808f88f7a374b7c7e03e82f8a05
Ahmed El Sayegh <ahmedelsayegh7@gmail.com>
2019-01-28 18:39:09 +0200

    First version of terminal cv

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Terminal from 'terminal-in-react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const commands = {
+  contact: () => `Name: Ahmed El Sayegh
+Email: ahmedelsayegh7@gmail.com
+Phone: +201120006617
+LinkedIn: https://www.linkedin.com/in/sayegh7/
+`,
+  github: () => { window.open('https://www.github.com/sayegh7', '_blank'); return 'done'},
 }
 
-export default App;
+const descriptions = {
+  contact: 'shows a message',
+  github: 'opens my github profile',
+}
+export default class App extends Component {
+  render () {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <Terminal
+          color='green'
+          backgroundColor='black'
+          barColor='black'
+          style={{ fontWeight: 'bold', fontSize: '1em' }}
+          commands={commands}
+          descriptions={descriptions}
+          msg="Hello, I'm Ahmed El Sayegh. Welcome to my portfolio. Type help for the available options" />
+      </div>
+    )
+  }
+}
