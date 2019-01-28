037c8863e0df95cab0ca959f4f40ae4050c0873f
Ahmed El Sayegh <ahmedelsayegh7@gmail.com>
2019-01-28 18:47:35 +0200

    Updates

---
+++ precache-manifest.305d1c64324a6a2a4bb9cc3f78ffd535.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "196866b5ed066333e3e1",
+    "url": "/static/js/main.196866b5.chunk.js"
+  },
+  {
+    "revision": "2e5de41ff1a876845dde",
+    "url": "/static/js/1.2e5de41f.chunk.js"
+  },
+  {
+    "revision": "196866b5ed066333e3e1",
+    "url": "/static/css/main.501e7824.chunk.css"
+  },
+  {
+    "revision": "daa98e9f2c36597a757f8ed91dc3b894",
+    "url": "/index.html"
+  }
+];
